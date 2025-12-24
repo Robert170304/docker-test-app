4b960c14278b99f9772135b60b9d2784e04a7eb4
robertmacwan17 <macwanrobert04@gmail.com>
2025-12-24 19:10:33 +0530

    feat: Bind server to all network interfaces (0.0.0.0)

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ const server = http.createServer(async (req, res) => {
     // still start server so healthcheck can show unready state; app will return DB errors on /db
   }
 
-  server.listen(3000, () => {
+  server.listen(3000, "0.0.0.0", () => {
     console.log('Server running on port 3000');
   });
 })();
